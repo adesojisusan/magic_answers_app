d054680661cac3b495a089dc2e2d8e3e668e024f
adesoji susan <adesojisusan.adeola@gmail.com>
2020-08-17 02:33:59 +0100

    images

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,47 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MagicAnswers());
 }
-class MagicAnswers extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey[900],
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey,
-          title: Center(
-            child:Text('Magic Answers')
-          ),
-        ),
+ class MagicAnswers extends StatefulWidget {
+   @override
+   _MagicAnswersState createState() => _MagicAnswersState();
+ }
 
-      ),
-    );
-  }
-}
+ class _MagicAnswersState extends State<MagicAnswers> {
+  int magicAnswers= 1;
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       home: Scaffold(
+         backgroundColor: Colors.blueGrey,
+         appBar: AppBar(
+           backgroundColor: Colors.blueGrey[900],
+         ),
+         body: Center(
+           child: Column(
+             children: [
+               Expanded(
+                   child: FlatButton(
+                     onPressed: (){
+                       setState(() {
+
+                       });
+                     },
+                     child: Image(
+                       image: AssetImage
+                         ('images/ball$magicAnswers.png'),
+                       ),
+                   ),
+               ),
+             ],
+           ),
+         ),
+       ),
+     );
+   }
+ }
 
